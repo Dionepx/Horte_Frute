f06a51676d2013be1993f11788292371f08ff72f
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-02-29 09:43:35 -0300

    Add files via upload

--- Ecommerce/script.js
+++ Ecommerce/script.js
@@ -1,3 +1,9 @@
+
+
+// QUANDO USAR {OBJETO} E [ARRAY] ?
+
+
+
 const data = {
      "nome": 'Horte Frute',
      "saldo": 0,
@@ -9,21 +15,21 @@ const data = {
                     "nome": "Banana",
                     "preco_venda": 1.40,
                     "preco_compra": 0.90,
-                    "frutas_quantidade": 20
+                    "quantidade": 20
                },
                {
                     "id": 2,
                     "nome": "Laranja",
                     "preco_venda": 2.30,
                     "preco_compra": 1.3,
-                    "frutas_quantidade": 40
+                    "quantidade": 40
                },
                {
                     "id": 3,
                     "nome": "Morango",
                     "preco_venda": 2.00,
                     "preco_compra": 1,
-                    "frutas_quantidade": 40
+                    "quantidade": 40
                },
           ],
 
@@ -33,69 +39,73 @@ const data = {
                     "nome": "Cenoura",
                     "preco_venda": 1.40,
                     "preco_compra": 0.90,
-                    "legumes_quantidade": 20
+                    "quantidade": 20
                },
                {
                     "id": 5,
                     "nome": "Brócolis",
                     "preco_venda": 1.40,
                     "preco_compra": 0.90,
-                    "legumes_quantidade": 20
+                    "quantidade": 20
                },
                {
                     "id": 6,
                     "nome": "Abobrinha",
                     "preco_venda": 1.40,
                     "preco_compra": 0.90,
-                    "legumes_quantidade": 20
+                    "quantidade": 20
                },
           ],
      },
 }
 
 // Funcoes
-function venda(fruta, legume, frutas_qtd, legumes_qtd) {
-     let venda = 0
 
-     for (let i = 0; i < data.produtos.length; i++) {
-          const elemento = data.produtos[i];
+function venda(array, nome, qtd) {
+     let venda = 0;
 
-          if (elemento.nome === fruta || elemento.nome === legume) {
-               if (frutas_qtd > elemento.frutas_quantidade && legumes_qtd > elemento.legumes_quantidade || frutas_qtd && legumes_qtd) {
-                    return 'Não é possível vender'
-               }
+     for (let i = 0; i < array.length; i++) {
+          const elemento = array[i];
 
-               venda = elemento.preco_venda * frutas_qtd || elemento.preco_venda * legumes_qtd
-               elemento.legumes_quantidade -= frutas_qtd || legumes_qtd
-               data.saldo += venda
+          if (elemento.nome === nome) {
+               if (qtd > elemento.quantidade || qtd <= 0) {
+                    return 'Não é possível vender';
+               };
+
+               venda = elemento.preco_venda * qtd;
+               elemento.quantidade -= qtd;
+               data.saldo += venda;
           }
      }
 
-     return `Venda concluída: R$ ${venda.toFixed(2)}`
+     return `Venda concluída: R$ ${venda.toFixed(2)}`;
 }
 
-// function compra(fruta, legume, qtd) {
-//      let compra = 0
 
-//      for (let i = 0; i < data.produtos.length; i++) {
-//           const elemento = data.produtos[i];
 
-//           if (elemento.nome === fruta || elemento.nome === legume) {
-//                compra = elemento.preco_compra * qtd
-//                elemento.quantidade += qtd
-//                data.saldo += compra
-//           }
-//      }
+function compra(array, nome, qtd) {
+     let compra = 0;
 
-//      return `Compra concluída: R$${compra.toFixed(2)}`
-// }
+     for (let i = 0; i < array.length; i++) {
+          const elemento = array[i];
 
-// Chamada de venda
-const venda1 = venda("Banana", "Cenoura", 5, 5)
+          if (elemento.nome === nome) {
+               compra = elemento.preco_compra * qtd;
+               elemento.quantidade += qtd;
+               data.saldo += compra;
+          }
 
-// Chamada de compra
-// const compra1 = compra("Banana", 5)
+          return `Compra concluída: R$ ${compra.toFixed(2)}`;
+     }
 
-// Saida final
-console.log(venda1)
-console.log(data)
+//Chamada de venda 
+const venda1 = venda(data.produtos.legumes, "Cenoura", 5);
+
+//Chamada de compra
+const compra1 = compra(data.produtos.frutas, "Banana", 5);
+
+//Saida final
+// console.log(venda1)
+// console.log(compra1)
+
+// console.log(data.produtos.frutas)
