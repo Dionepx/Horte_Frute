1c9dc4db472ea53df9451f2aa395f7365cd07d11
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-03-02 16:05:10 -0300

    refatoracao

---
+++ Ecommerce/data.js
@@ -0,0 +1,110 @@
+export const data = {
+     "nome": 'Horte Frute',
+     "saldo": 0,
+
+     "produtos": {
+          "frutas": [
+               {
+                    "URL": "https://imgs.search.brave.com/VXHNQuW-asIW6Qog0gd9B_hjRkneXnB2YJ4rT_GGi5o/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTcz/MjQyNzUwL3Bob3Rv/L2JhbmFuYS1idW5j/aC5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9TUFjOEFYVno1/S3h3V2VFbWg3NVd3/SDZqX0hvdVJjekJG/QWh1bExBdFJVVT0",
+                    "id": 1,
+                    "nome": "Banana",
+                    "preco_venda": 1.40,
+                    "preco_compra": 0.90,
+                    "quantidade": 20
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/ZZzvBgfzh7Z9efLHW2yZSsELDCluVIkBFZYhgf2CCt0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jb250/ZXVkby5pbWd1b2wu/Y29tLmJyL2MvZW50/cmV0ZW5pbWVudG8v/MTMvMjAxOC8wNS8w/Mi9sYXJhbmphLTE1/MjUyOTQzOTIyMTFf/djJfNzUweDQyMS5q/cGc",
+                    "id": 2,
+                    "nome": "Laranja",
+                    "preco_venda": 2.30,
+                    "preco_compra": 1.3,
+                    "quantidade": 40
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/csBVXghG9EjRzw-LrO4cq2Aj-Aq_pHCOTMtVUvBSMbo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9kaWdp/dGFsLmFncmlzaG93/LmNvbS5ici9zaXRl/cy9hZ3Jpc2hvdy5j/b20vZmlsZXMvc3R5/bGVzL2FydGljbGVf/ZmVhdHVyZWRfc3Rh/bmRhcmQvcHVibGlj/L1BhZHJvLUFydGln/by1HQ1AtNzcweDQw/MCUyMC0lMjAyMDIy/LTA4LTA4VDE4MzUw/MC41MzkuanBnP2l0/b2s9Uk9VLXJsNE4",
+                    "id": 3,
+                    "nome": "Morango",
+                    "preco_venda": 2.00,
+                    "preco_compra": 1,
+                    "quantidade": 40
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/6ZkV0wAWNW-ZIBovUFuHOrlKtGhAGmstv9E0e9tB0Vs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2xhcmdlLXByZXZp/ZXdzLzVlYi91dmEt/MTUyNDkyMy5qcGc_/Zm10",
+                    "id": 4,
+                    "nome": "Uva",
+                    "preco_venda": 2.00,
+                    "preco_compra": 1,
+                    "quantidade": 40
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/EXJmXXY9Fj1y1pfs75KV6U4DTBQW-OwF5EF0tGL3fnE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9mZWVk/LmNvbnRpbmVudGUu/cHQvbWVkaWEvcjI1/Y2ZmMGovbWFjYS1n/YWxhLTE2MDB4OTAw/LmpwZz9hbmNob3I9/Y2VudGVyJm1vZGU9/Y3JvcCZ3aWR0aD0x/NjgwJmhlaWdodD03/MjAmcm5kPTEzMzA3/MDE0MzczODIwMDAw/MCZmb3JtYXQ9d2Vi/cA",
+                    "id": 5,
+                    "nome": "Maçã",
+                    "preco_venda": 2.00,
+                    "preco_compra": 1,
+                    "quantidade": 40
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/2GBFRbdY5-T9gogGldhTRZQtjC8Gc-RVjMaWiUT-r5w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy81/LzVkL1BlYXJfRFMu/anBn",
+                    "id": 6,
+                    "nome": "Pêra",
+                    "preco_venda": 2.00,
+                    "preco_compra": 1,
+                    "quantidade": 40
+               },
+
+          ],
+
+          "legumes": [
+               {
+                    "URL": "https://imgs.search.brave.com/n1yerl86iWSe00db0upna4meXCtpEfNdjm0_jOPxJ7g/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/YXdzbGkuY29tLmJy/LzYwMHg3MDAvMzA1/LzMwNTkxMy9wcm9k/dXRvLzEwMjkzNTUy/L2Nlbm91cmEtNjNk/NTFjMWMuanBn",
+                    "id": 7,
+                    "nome": "Cenoura",
+                    "preco_venda": 1.40,
+                    "preco_compra": 0.90,
+                    "quantidade": 20
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/vhXwipVXICp6Ag34-UACGoQmzMv5CK9GWVgqfdG5d9w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMubXVuZG9lZHVj/YWNhby51b2wuY29t/LmJyL211bmRvZWR1/Y2FjYW8vY29udGV1/ZG9fbGVnZW5kYS85/MWI2MTJmNmYxMWNi/ODY0NmQyNDQyNGNi/YmYyZjQ2Zi5qcGc",
+                    "id": 8,
+                    "nome": "Brócolis",
+                    "preco_venda": 1.40,
+                    "preco_compra": 0.90,
+                    "quantidade": 20
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/aEyKRFTb3nQ-qx6bZyNZGn9G7039ctPNyGnTrVUBuUI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zMS5z/dGF0aWMuYnJhc2ls/ZXNjb2xhLnVvbC5j/b20uYnIvYmUvY29u/dGV1ZG8vaW1hZ2Vz/L2Fib2JyaW5oYS5q/cGc",
+                    "id": 9,
+                    "nome": "Abobrinha",
+                    "preco_venda": 1.40,
+                    "preco_compra": 0.90,
+                    "quantidade": 20
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/2JHrgZ7NJJLz6Un_BeX9wYhgwhv9psK0n94fvkVAalA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jb250/ZXVkby5pbWd1b2wu/Y29tLmJyL2MvZW50/cmV0ZW5pbWVudG8v/YTAvMjAxOC8xMS8w/Mi9iYXRhdGEtMTAt/MTU0MTE4NTY3NDEx/MF92Ml83NTB4NDIx/LmpwZw",
+                    "id": 10,
+                    "nome": "Batata",
+                    "preco_venda": 1.40,
+                    "preco_compra": 0.90,
+                    "quantidade": 20
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/JT1e8hYQvIPu2CHQjI4LsYPFK-wCDuXUlrZnsoUfQcg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZWN5Y2xlLmNv/bS5ici93cC1jb250/ZW50L3VwbG9hZHMv/MjAxNS8xMi8wMTE4/NTMzOC9BaXBpbURT/QzcyNDEtMTAyNHg2/ODEuanBnLndlYnA",
+                    "id": 11,
+                    "nome": "Aipim",
+                    "preco_venda": 1.40,
+                    "preco_compra": 0.90,
+                    "quantidade": 20
+               },
+               {
+                    "URL": "https://imgs.search.brave.com/BPmmWl1vwJjUoC3YCL8ntfivj2m6VUt4H_pmkaoHH_c/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/dHJlbmRzYnIuY29t/LmJyL3VwbG9hZHMv/bm90aWNpYXMvMjAy/MS8wMi8yMi8zMWZu/YWFzNGJjdzAwLmpw/Zw",
+                    "id": 12,
+                    "nome": "Couve-flor",
+                    "preco_venda": 1.40,
+                    "preco_compra": 0.90,
+                    "quantidade": 20
+               }
+          ],
+     },
+}
+
--- Ecommerce/script.js
+++ Ecommerce/script.js
@@ -1,101 +1,26 @@
-const data = {
-     "nome": 'Horte Frute',
-     "saldo": 0,
+import { data } from "./data.js";
 
-     "produtos": {
-          "frutas": [
-               {
-                    "id": 1,
-                    "nome": "Banana",
-                    "preco_venda": 1.40,
-                    "preco_compra": 0.90,
-                    "quantidade": 20
-               },
-               {
-                    "id": 2,
-                    "nome": "Laranja",
-                    "preco_venda": 2.30,
-                    "preco_compra": 1.3,
-                    "quantidade": 40
-               },
-               {
-                    "id": 3,
-                    "nome": "Morango",
-                    "preco_venda": 2.00,
-                    "preco_compra": 1,
-                    "quantidade": 40
-               },
-          ],
+const divFrutas = document.getElementById('frutas');
+const divLegumes = document.getElementById('legumes');
+const searchBar = document.getElementById('search');
+const buttonB = document.getElementById('button')
 
-          "legumes": [
-               {
-                    "id": 4,
-                    "nome": "Cenoura",
-                    "preco_venda": 1.40,
-                    "preco_compra": 0.90,
-                    "quantidade": 20
-               },
-               {
-                    "id": 5,
-                    "nome": "Brócolis",
-                    "preco_venda": 1.40,
-                    "preco_compra": 0.90,
-                    "quantidade": 20
-               },
-               {
-                    "id": 6,
-                    "nome": "Abobrinha",
-                    "preco_venda": 1.40,
-                    "preco_compra": 0.90,
-                    "quantidade": 20
-               },
-          ],
-     },
-}
-
-// Funcoes
-
-function venda(array, nome, qtd) {
-     let venda = 0;
-
-     for (let i = 0; i < array.length; i++) {
-          const elemento = array[i];
-
-          if (elemento.nome === nome) {
-               if (qtd > elemento.quantidade || qtd <= 0) {
-                    return 'Não é possível vender';
-               };
-
-               venda = elemento.preco_venda * qtd;
-               elemento.quantidade -= qtd;
-               data.saldo += venda;
-          }
-     }
-
-     return `Venda concluída: R$ ${venda.toFixed(2)}`;
-}
+// data is an object, not an array. So, data.map() wont work
+data.produtos.frutas.map(el => divFrutas.innerHTML += `
+     <div class='container-fruta'>
+          <img src='${el.URL}'>
+          <h4>Número: ${el.id}</h4>
+          <h2>${el.nome}</h2>
+          <h1>R$ ${el.preco_venda.toFixed(2)}<h1>
+     </div>
+`);
 
 
-
-function compra(array, nome, qtd) {
-     let compra = 0;
-
-     for (let i = 0; i < array.length; i++) {
-          const elemento = array[i];
-
-          if (elemento.nome === nome) {
-               compra = elemento.preco_compra * qtd;
-               elemento.quantidade += qtd;
-               data.saldo += compra;
-          }
-
-          return `Compra concluída: R$ ${compra.toFixed(2)}`;
-     }
-
-//Chamada de venda 
-const venda1 = venda(data.produtos.legumes, "Cenoura", 5);
-
-//Chamada de compra
-const compra1 = compra(data.produtos.frutas, "Banana", 5);
-
-//Saida final
+data.produtos.legumes.map(el => divLegumes.innerHTML += `
+<div class='container-fruta'>
+     <img src='${el.URL}'>
+     <h4>Número: ${el.id}</h4>
+     <h2>${el.nome}</h2>
+     <h1>R$ ${el.preco_venda.toFixed(2)}<h1>
+</div>
+`)
