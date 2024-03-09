0e8f5ecdac7e7fe21c7b75d8e7cf9324e8839e42
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-03-08 21:03:55 -0300

    Add files via upload

--- Ecommerce/script.js
+++ Ecommerce/script.js
@@ -1,26 +1,43 @@
 import { data } from "./data.js";
+const { produtos: {frutas, legumes} } = data
 
 const divFrutas = document.getElementById('frutas');
 const divLegumes = document.getElementById('legumes');
-const searchBar = document.getElementById('search');
-const buttonB = document.getElementById('button')
-
-// data is an object, not an array. So, data.map() wont work
-data.produtos.frutas.map(el => divFrutas.innerHTML += `
-     <div class='container-fruta'>
-          <img src='${el.URL}'>
-          <h4>Número: ${el.id}</h4>
-          <h2>${el.nome}</h2>
-          <h1>R$ ${el.preco_venda.toFixed(2)}<h1>
-     </div>
-`);
+const divResultados = document.getElementById('resultados');
+const search = document.getElementById('searchBar');
+const buttonSearch = document.getElementById('btn');
 
 
-data.produtos.legumes.map(el => divLegumes.innerHTML += `
-<div class='container-fruta'>
-     <img src='${el.URL}'>
-     <h4>Número: ${el.id}</h4>
-     <h2>${el.nome}</h2>
-     <h1>R$ ${el.preco_venda.toFixed(2)}<h1>
-</div>
-`)
+function createElement(array, div) {
+     array.map(el => div.innerHTML += `
+          <div class='text-center d-flex flex-column'>
+          <div>
+               <img src='${el.URL}'>
+          </div>
+               <h4>Número: ${el.id}</h4>
+               <h2>${el.nome}</h2>
+               <h1>R$ ${el.preco_venda.toFixed(2)}</h1>
+          </div>
+     `)
+};
+
+function searchElement() {
+     const filterFrutas = frutas.filter(el => search.value === el.nome);
+     const filterLegumes = legumes.filter(el => search.value === el.nome);
+     const newArray = [...filterFrutas, ...filterLegumes];
+     divFrutas.innerHTML = ''
+     divLegumes.innerHTML = ''
+     divResultados.innerHTML = ''
+     createElement(newArray, divResultados);
+};
+
+function app() {
+     createElement(frutas, divFrutas);
+     createElement(legumes, divLegumes);
+};
+
+// DUVIDA
+// Se eu comentar o window, os produtos desaparecem por que?
+
+window.addEventListener('load', app); // recarrega primeiro HTML e depois o JS
+buttonSearch.addEventListener('click', searchElement); 
